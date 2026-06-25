7d534656e78e75a66103cd7a407e900cf3cba6cc
popytka008 <sasha_1834b@mail.ru>
2026-06-26 01:10:20 +0300

    Завершение изучения системы безопасности.

--- src/main/java/org/example/crud_employ/security/MySecurityConfiguration.java
+++ src/main/java/org/example/crud_employ/security/MySecurityConfiguration.java
@@ -2,37 +2,68 @@ package org.example.crud_employ.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 public class MySecurityConfiguration {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManage(){
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager( dataSource );
+    }
+//
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManage(){
+//
+//        UserDetails details1 = User.builder()
+//                .username("ivan")
+//                .password("{noop}test")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails details2 = User.builder()
+//                .username("oleg")
+//                .password("{noop}test")
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//
+//        UserDetails details3 = User.builder()
+//                .username("inna")
+//                .password("{noop}test")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(
+//                details1, details2, details3
+//        );
+//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        UserDetails details1 = User.builder()
-                .username("ivan")
-                .password("{noop}test")
-                .roles("EMPLOYEE")
-                .build();
+        http.authorizeHttpRequests( config ->
+                config
+            .requestMatchers( HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+            .requestMatchers( HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+            .requestMatchers( HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+            .requestMatchers( HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+            .requestMatchers( HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
 
-        UserDetails details2 = User.builder()
-                .username("oleg")
-                .password("{noop}test")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+        http.httpBasic( Customizer.withDefaults() );
+        http.csrf(AbstractHttpConfigurer::disable);
 
-        UserDetails details3 = User.builder()
-                .username("inna")
-                .password("{noop}test")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
 
-        return new InMemoryUserDetailsManager(
-                details1, details2, details3
-        );
+        return http.build();
     }
 }
